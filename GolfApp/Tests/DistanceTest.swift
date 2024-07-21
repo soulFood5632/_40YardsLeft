5c3577373b2d8a89e6864d3f4be29232cebe31b9
soulFood5632 <loganu@shaw.ca>
2024-07-21 14:29:29 -0700

    added back the simple unit tests

---
+++ GolfApp/Tests/DistanceTest.swift
@@ -0,0 +1,24 @@
+//
+//  Distance.swift
+//  _40YardsLeftTests
+//
+//  Created by Logan Underwood on 2023-04-07.
+//
+
+import XCTest
+@testable import _40YardsLeft
+
+final class DistanceTest: XCTestCase {
+
+    
+
+    
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
