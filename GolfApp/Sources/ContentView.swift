30ab946b6f44fe5b7bf245010a29afba49c3b27b
soulFood5632 <loganu@shaw.ca>
2024-07-21 14:27:54 -0700

    completed tuist migration

---
+++ GolfApp/Sources/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  _40YardsLeft
+//
+//  Created by Logan Underwood on 2023-03-14.
+//
+
+import CoreData
+import FirebaseAuth
+import SwiftUI
+
+struct ContentView: View {
+
+  @State private var userIsLoggedOut = false
+  @State private var path = NavigationPath()
+
+  var body: some View {
+    NavigationStack(path: $path) {
+
+      let _ = Self._printChanges()
+
+      WelcomeScreen(path: $path)
+        .navigationDestination(for: Golfer.self) { newGolfer in
+          HomeView(golfer: newGolfer, path: self.$path)
+
+        }
+
+    }
+    .onChange(of: path) { newPath in
+      print(newPath.count)
+    }
+
+  }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
